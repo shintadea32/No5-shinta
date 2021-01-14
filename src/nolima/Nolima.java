da8fe35272cfdf7081f87841a3ae7fa623266e50
shintadewi29rpl <shinta_dewi_29rpl@gmail.com>
2021-01-14 23:54:55 +0700

    Project Keempat

---
+++ src/nolima/Nolima.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nolima;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Nolima {
+    public static void main(String[] args) {
+      for(int i=1; i<=4; i++){
+            for (int j = 1; j <=4; j++){
+                if (i==1 || i==4 || j==1 ||j==4){
+                    System.out.print("@");
+                }else{
+                    System.out.print(" ");
+                }
+                
+   }System.out.println("");
+        }
+    }
+    
+}
